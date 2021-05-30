501b7af055d1b108f72b49e200655172b8d7e110
Denis Korchagin <denis.korchagin.1995@gmail.com>
2021-05-30 17:27:34 +0300

    feat(03-streams): finish the task '02-line-split-stream'

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -2,14 +2,33 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
+  #remain = '';
+
   constructor(options) {
     super(options);
   }
 
   _transform(chunk, encoding, callback) {
+    let string = chunk.toString(this.encoding);
+    let strings = (this.#remain + string).split(os.EOL);
+    this.#remain = strings.pop();
+    if (this.#remain == null) {
+      this.#remain = '';
+    }
+
+    for(let i = 0, len = strings.length; i < len; ++i) {
+      this.push(strings[i]);
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    if (this.#remain.length > 0) {
+      this.push(this.#remain);
+      this.#remain = '';
+    }
+    callback();
   }
 }
 
